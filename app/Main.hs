b52ec96f2a8bb5d61d97732e69ff65505ee893c8
Yuji Yokoo <yujiyokoo@gmail.com>
2016-06-16 16:08:54 +0000

    Initial commit for public project

---
+++ app/Main.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
+-- (with stackage) you can run this like
+-- > stack exec search-download-exe -- --key=<youtube api key> --keyword=<search keyword> [--format=mp3]
+
+module Main where
+
+import Lib
+import System.Console.CmdArgs
+import Control.Monad
+import qualified Network.HTTP as H (urlEncode)
+import System.Process
+
+data Args =
+  Args { keyword     :: String
+       , key         :: String
+       , format :: String
+       } deriving (Show, Data, Typeable)
+
+cmdargs = Args { keyword = def,
+                 key = def,
+                 format = def }
+
+options :: String -> [String]
+options audioFormat = case audioFormat of
+  "mp3" -> ["-x", "--audio-format=mp3"]
+  _ -> []
+
+main :: IO ()
+
+main = do
+  a <- cmdArgs cmdargs
+  let url = (searchUrl (keyword a) (key a))
+  putStrLn ("searching " ++ url)
+  results_json <- callSearch url
+  let results = decodeSearchResults results_json
+  let vidId = (liftM getOneId) results
+  let videoUrl = liftM2 (++) (Right "https://www.youtube.com/watch?v=") ((liftM H.urlEncode) vidId)
+  let message = liftM2 (++) (Right "downloading ") videoUrl
+  putStrLn (show message)
+  let actualUrl = either (const "") id videoUrl
+  output <- readProcess "youtube-dl" (options (format a) ++ [actualUrl]) []
+  putStrLn (output)
+  -- callSearch url >>= putStrLn . show . decodeSearchResults
---
+++ src/Lib.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
+
+module Lib
+    ( callSearch
+    , searchUrl
+    , decodeSearchResults
+    , getOneId
+
+    , smallTest
+
+    ) where
+
+import qualified Data.ByteString.Lazy as B
+import Network.HTTP.Conduit (simpleHttp)
+import qualified Network.HTTP as H (urlEncode)
+import Data.Aeson (decode, eitherDecode)
+import Data.Aeson.Types
+import Data.Text
+import Control.Monad
+import GHC.Generics
+
+--------------------------------------
+
+searchUrl :: String -> String -> String
+searchUrl query api_key =
+  "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" ++
+    (H.urlEncode query) ++
+    "&key=" ++
+    (H.urlEncode api_key)
+
+callSearch :: String -> IO B.ByteString
+callSearch search_url = simpleHttp search_url
+
+decodeSearchResults :: B.ByteString -> Either String SnippetResults
+decodeSearchResults = eitherDecode
+
+smallTest x = eitherDecode x :: Either String SnippetResults
+
+-- This should switch to using monad for errors
+getOneId :: SnippetResults -> String
+getOneId (SnippetResults [])  = ""
+getOneId (SnippetResults (x:xs)) = videoId x
+
+data SnippetResults = SnippetResults [ SnippetResult ] deriving Show
+data SnippetResult =
+  SnippetResult { title       :: String
+                , description :: String
+                , kind        :: String
+                , videoId     :: String
+                } deriving Show
+
+-- For nested object parsing: http://stackoverflow.com/questions/24742872/parsing-an-array-with-haskell-aeson#24743369
+instance FromJSON SnippetResults where
+  parseJSON jsn = case jsn of
+    Object v -> (v .: "items") >>= fmap SnippetResults . parseJSON
+    x -> fail $ "unexpected json: " ++ show x
+
+instance FromJSON SnippetResult where
+  parseJSON (Object v) = do
+    s <- v .: "snippet"
+    t <- s .: "title"
+    d <- s .: "description"
+    id  <- v .: "id"
+    k   <- id .: "kind"
+    vid <- id .: "videoId"
+    return SnippetResult { title = t, description = d, kind = k, videoId = vid }
+
